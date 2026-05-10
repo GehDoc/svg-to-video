a9681b967a011484f806cd6ec37e1ed1b0cb2f07
Gérald Hameau <GehDoc@users.noreply.github.com>
2026-05-10 11:17:00 +0200

    test: restore WithSvg story and add new transparency stories

--- web/src/components/ConfigPanel.stories.tsx
+++ web/src/components/ConfigPanel.stories.tsx
@@ -39,6 +39,44 @@ export const WithSvg: Story = {
   ],
 };
 
+export const TransparentEnabled: Story = {
+  decorators: [
+    (Story) => (
+      <MockStudioProvider
+        mockValues={{
+          svgContent: '<svg></svg>',
+          format: 'webm',
+          isTransparent: true,
+          originalDim: { width: 500, height: 500, isDimensionsDetected: true },
+        }}
+      >
+        <div className="story-wrapper">
+          <Story />
+        </div>
+      </MockStudioProvider>
+    ),
+  ],
+};
+
+export const TransparentDisabled: Story = {
+  decorators: [
+    (Story) => (
+      <MockStudioProvider
+        mockValues={{
+          svgContent: '<svg></svg>',
+          format: 'mp4',
+          isTransparent: false,
+          originalDim: { width: 500, height: 500, isDimensionsDetected: true },
+        }}
+      >
+        <div className="story-wrapper">
+          <Story />
+        </div>
+      </MockStudioProvider>
+    ),
+  ],
+};
+
 export const Rendering: Story = {
   decorators: [
     (Story) => (
