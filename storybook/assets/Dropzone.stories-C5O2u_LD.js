759099dabdbc5b327585fb351f18cfc4ae7e0f99
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-05-14 06:45:16 +0000

    Deploying to gh-pages from @ GehDoc/svg-to-video@90e00f609297625f0db9b8468113b2cac7af1e98 🚀

--- storybook/assets/Dropzone.stories-C5O2u_LD.js
+++ storybook/assets/Dropzone.stories-C5O2u_LD.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./Dropzone-DeoCOrNX.js";var r,i,a,o,s,c;e((()=>{t(),r={title:`Components/Dropzone`,component:n},i={args:{svgContent:null,isDragging:!1}},a={args:{svgContent:`<svg></svg>`,isDragging:!1}},o={args:{svgContent:null,disabled:!0}},s={args:{svgContent:`<svg></svg>`,disabled:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./Dropzone-lIaph9zh.js";var r,i,a,o,s,c;e((()=>{t(),r={title:`Components/Dropzone`,component:n},i={args:{svgContent:null,isDragging:!1}},a={args:{svgContent:`<svg></svg>`,isDragging:!1}},o={args:{svgContent:null,disabled:!0}},s={args:{svgContent:`<svg></svg>`,disabled:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
   args: {
     svgContent: null,
     isDragging: false
--- storybook/assets/MetaDisplay.stories-BbTfA9mf.js
+++ storybook/assets/MetaDisplay.stories-BbTfA9mf.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./MetaDisplay-C-3QVbcA.js";var r,i,a,o,s;e((()=>{t(),r={title:`Components/MetaDisplay`,component:n,tags:[`autodocs`]},i={args:{meta:{originalSize:`100x100`,finalSize:`1920x1080`,codec:`h264`,eta:5}}},a={args:{dimensions:{width:500,height:500,targetWidth:1080,targetHeight:1080}}},o={args:{}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./MetaDisplay-_lthta4o.js";var r,i,a,o,s;e((()=>{t(),r={title:`Components/MetaDisplay`,component:n,tags:[`autodocs`]},i={args:{meta:{originalSize:`100x100`,finalSize:`1920x1080`,codec:`h264`,eta:5}}},a={args:{dimensions:{width:500,height:500,targetWidth:1080,targetHeight:1080}}},o={args:{}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
   args: {
     meta: {
       originalSize: '100x100',
