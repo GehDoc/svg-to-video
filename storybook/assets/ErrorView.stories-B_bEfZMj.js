52660c347b6c58a3fd3b8a451fee352abaa75c47
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-05-15 21:52:46 +0000

    Deploying to gh-pages from @ GehDoc/svg-to-video@696758d60f7d7678dbd87df8995833a70c16b6e6 🚀

--- storybook/assets/ErrorView.stories-B_bEfZMj.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./ErrorView-3PLmJg-A.js";var r,i,a,o,s;e((()=>{t(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/ErrorView`,component:n,tags:[`autodocs`],args:{onClose:r()}},a={args:{message:`An unexpected error occurred while rendering the video.`}},o={args:{message:`Invalid SVG content.`}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'An unexpected error occurred while rendering the video.'
-  }
-}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Invalid SVG content.'
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Default`,`ShortMessage`]}))();export{a as Default,o as ShortMessage,s as __namedExportsOrder,i as default};
