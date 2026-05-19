be10379805649dc58d2003cea491e169f21418f6
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-05-19 06:25:47 +0000

    Deploying to gh-pages from @ GehDoc/svg-to-video@740c76d2a6ab39928cc3ab10dd0a710a35733192 🚀

---
+++ storybook/assets/Button.stories-DJxXeQMI.js
@@ -0,0 +1,22 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./Button-CFRz17Ws.js";var r,i,a,o,s,c;e((()=>{t(),r={title:`Components/Button`,component:n,tags:[`autodocs`],argTypes:{variant:{control:`select`,options:[`primary`,`secondary`,`error`]},onClick:{action:`clicked`}}},i={args:{variant:`primary`,children:`Primary Button`}},a={args:{variant:`secondary`,children:`Secondary Button`}},o={args:{variant:`error`,children:`Error Button`}},s={args:{variant:`primary`,disabled:!0,children:`Disabled Button`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    children: 'Primary Button'
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'error',
+    children: 'Error Button'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    disabled: true,
+    children: 'Disabled Button'
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Primary`,`Secondary`,`Error`,`Disabled`]}))();export{s as Disabled,o as Error,i as Primary,a as Secondary,c as __namedExportsOrder,r as default};
