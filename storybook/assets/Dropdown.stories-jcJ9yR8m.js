01d099a1045e3d68e90a709ee21a7a36a8ddb0fc
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-05-20 22:35:06 +0000

    Deploying to gh-pages from @ GehDoc/svg-to-video@aeead10f8da37e8f78eb5a3937e3e163d32fa956 🚀

---
+++ storybook/assets/Dropdown.stories-jcJ9yR8m.js
@@ -0,0 +1,53 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-DxP0NviS.js";import{n,t as r}from"./Dropdown-DejTqg5r.js";import{c as i,d as a,o,p as s,r as c,s as l,t as u}from"./fa-B2a3_xYo.js";var d,f,p,m;e((()=>{n(),s(),d=t(),f={title:`Components/Dropdown`,component:r},p={args:{sections:[{label:`Actions`,items:[{label:`Copy as Data URL`,onClick:()=>console.log(`Data URL`),icon:(0,d.jsx)(o,{})},{label:`Copy as Video File`,onClick:()=>console.log(`Video File`),icon:(0,d.jsx)(l,{})}]},{label:`Project`,items:[{label:`View Source Code`,href:`https://github.com/GehDoc/svg-to-video`,target:`_blank`,icon:(0,d.jsx)(i,{})},{label:`Report an Issue`,href:`https://github.com/GehDoc/svg-to-video/issues`,target:`_blank`,icon:(0,d.jsx)(c,{})}]},{label:`About`,items:[{label:`Version`,isMeta:!0,icon:(0,d.jsx)(a,{}),rightElement:(0,d.jsxs)(`span`,{className:`dropdown-version-tag`,children:[`v0.11.3 `,(0,d.jsx)(`span`,{className:`dropdown-arrow`,children:`↗`})]})},{label:`License`,isMeta:!0,icon:(0,d.jsx)(u,{}),rightElement:(0,d.jsxs)(`span`,{className:`dropdown-version-tag`,children:[`MIT `,(0,d.jsx)(`span`,{className:`dropdown-arrow`,children:`↗`})]})}]}]},decorators:[e=>(0,d.jsx)(`div`,{style:{position:`relative`,width:`240px`,padding:`2rem`},children:(0,d.jsx)(e,{})})]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    sections: [{
+      label: 'Actions',
+      items: [{
+        label: 'Copy as Data URL',
+        onClick: () => console.log('Data URL'),
+        icon: <FaCopy />
+      }, {
+        label: 'Copy as Video File',
+        onClick: () => console.log('Video File'),
+        icon: <FaFile />
+      }]
+    }, {
+      label: 'Project',
+      items: [{
+        label: 'View Source Code',
+        href: 'https://github.com/GehDoc/svg-to-video',
+        target: '_blank',
+        icon: <FaGithub />
+      }, {
+        label: 'Report an Issue',
+        href: 'https://github.com/GehDoc/svg-to-video/issues',
+        target: '_blank',
+        icon: <FaBug />
+      }]
+    }, {
+      label: 'About',
+      items: [{
+        label: 'Version',
+        isMeta: true,
+        icon: <FaTag />,
+        rightElement: <span className="dropdown-version-tag">
+                v0.11.3 <span className="dropdown-arrow">↗</span>
+              </span>
+      }, {
+        label: 'License',
+        isMeta: true,
+        icon: <FaBalanceScale />,
+        rightElement: <span className="dropdown-version-tag">
+                MIT <span className="dropdown-arrow">↗</span>
+              </span>
+      }]
+    }]
+  },
+  decorators: [Story => <div style={{
+    position: 'relative',
+    width: '240px',
+    padding: '2rem'
+  }}>
+        <Story />
+      </div>]
+}`,...p.parameters?.docs?.source}}},m=[`FullCapability`]}))();export{p as FullCapability,m as __namedExportsOrder,f as default};
