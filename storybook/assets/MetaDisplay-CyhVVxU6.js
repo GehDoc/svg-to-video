1053cab33cda617d733b36689ef937491107c6f0
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-05-27 23:14:27 +0000

    Deploying to gh-pages from @ GehDoc/svg-to-video@ad81e3c029ab69e00bf594c9d7b9116a563edb34 🚀

---
+++ storybook/assets/MetaDisplay-CyhVVxU6.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-DxP0NviS.js";var n=e((()=>{})),r,i,a=e((()=>{n(),r=t(),i=({meta:e,dimensions:t})=>{let n=e?.originalSize||(t?`${t.width}x${t.height}`:`---`),i=e?.finalSize||(t?`${t.targetWidth}x${t.targetHeight}`:`---`),a=e?.codec||`---`,o=e?.eta===void 0?`---`:`${e.eta}s`;return(0,r.jsxs)(`div`,{className:`meta-grid`,children:[(0,r.jsxs)(`div`,{className:`meta-item`,children:[(0,r.jsx)(`strong`,{children:`Source:`}),` `,n]}),(0,r.jsxs)(`div`,{className:`meta-item`,children:[(0,r.jsx)(`strong`,{children:`Export:`}),` `,i]}),(0,r.jsxs)(`div`,{className:`meta-item`,children:[(0,r.jsx)(`strong`,{children:`Codec:`}),` `,a]}),(0,r.jsxs)(`div`,{className:`meta-item`,children:[(0,r.jsx)(`strong`,{children:`ETA:`}),` `,o]})]})},i.__docgenInfo={description:``,methods:[],displayName:`MetaDisplay`,props:{meta:{required:!1,tsType:{name:`signature`,type:`object`,raw:`{
+  originalSize: string;
+  finalSize: string;
+  codec: string;
+  eta: number;
+}`,signature:{properties:[{key:`originalSize`,value:{name:`string`,required:!0}},{key:`finalSize`,value:{name:`string`,required:!0}},{key:`codec`,value:{name:`string`,required:!0}},{key:`eta`,value:{name:`number`,required:!0}}]}},description:``},dimensions:{required:!1,tsType:{name:`signature`,type:`object`,raw:`{
+  width: number;
+  height: number;
+  targetWidth: number;
+  targetHeight: number;
+}`,signature:{properties:[{key:`width`,value:{name:`number`,required:!0}},{key:`height`,value:{name:`number`,required:!0}},{key:`targetWidth`,value:{name:`number`,required:!0}},{key:`targetHeight`,value:{name:`number`,required:!0}}]}},description:``}}}}));export{a as n,i as t};
